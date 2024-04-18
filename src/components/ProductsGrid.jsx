eca44dadf158c151ad3dbfba9a41d39cef468a9e
Adelya <msvadelya@gmail.com>
2024-04-19 04:27:42 +0600

    fixed bugs in products card

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -1,6 +1,6 @@
 function ProductsGrid({ children }) {
   return (
-    <div className="grid grid-cols-1 gap-10 mt-10 md:grid-cols-2 lg:grid-cols-4">
+    <div className="grid items-center grid-cols-1 gap-10 mt-10 md:grid-cols-2 lg:grid-cols-4">
       {children}
     </div>
   );
