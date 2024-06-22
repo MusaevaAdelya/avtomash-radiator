5e05488f0ec65aa1d4a358b10c9bc2374fb90699
nikonych <mamixkik@gmail.com>
2024-06-22 09:03:39 +0600

    BASE_URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // const BASE_URL = 'https://nikonych.pythonanywhere.com';
-// const BASE_URL = 'http://jskbashat.kg:8000';
-const BASE_URL = 'http://0.0.0.0:8000';
+const BASE_URL = 'http://jskbashat.kg:8000';
+// const BASE_URL = 'http://0.0.0.0:8000';
 
 export { BASE_URL };
